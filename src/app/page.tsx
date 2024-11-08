ec7e73b2358289030a74a7aeb28e0250dbaba0ab
Nicholas Robert Beckham <nibih@live.com>
2024-11-09 03:51:40 +0700

    Add current time display to home page with real-time update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 export default function Home() {
+  // time at center
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString());
+  }, 1000);
+
   return (
     <>
-      <BackgroundGradientAnimation></BackgroundGradientAnimation>
+      <BackgroundGradientAnimation>
+        {/* current time at center */}
+        <div className="flex items-center justify-center h-full text-4xl font-bold text-white">
+          {time}
+        </div>
+      </BackgroundGradientAnimation>
     </>
   );
 }
